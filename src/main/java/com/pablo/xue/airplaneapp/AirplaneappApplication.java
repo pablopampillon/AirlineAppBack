65558f0e32a83e1665d406fbf93a779421df9ec2
Pablo Pampillon <pablo@pampillon.com>
2022-11-29 11:41:25 +0100

    first commit

---
+++ src/main/java/com/pablo/xue/airplaneapp/AirplaneappApplication.java
@@ -0,0 +1,13 @@
+package com.pablo.xue.airplaneapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplaneappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirplaneappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablo/xue/airplaneapp/AirplaneappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo.xue.airplaneapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirplaneappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
