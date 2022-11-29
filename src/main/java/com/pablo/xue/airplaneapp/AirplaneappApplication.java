421caec3f45f2ec9ca3118c2f02eac35e0bc5f03
Xuebo Zhu <xbzh1994@gmail.com>
2022-11-29 14:18:05 +0100

    controller vuelos y users

--- src/main/java/com/pablo/xue/airplaneapp/AirplaneappApplication.java
+++ src/main/java/com/pablo/xue/airplaneapp/AirplaneappApplication.java
@@ -1,5 +1,7 @@
 package com.pablo.xue.airplaneapp;
 
+import java.util.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
