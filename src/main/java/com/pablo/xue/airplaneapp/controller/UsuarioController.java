c834bb9ebb84dc21755cc9786f356cd222c4c02a
Xuebo Zhu <xbzh1994@gmail.com>
2022-11-29 16:12:54 +0100

    filter vuelos by Origen, Destino y fecha Ini

--- src/main/java/com/pablo/xue/airplaneapp/controller/UsuarioController.java
+++ src/main/java/com/pablo/xue/airplaneapp/controller/UsuarioController.java
@@ -14,7 +14,6 @@ import com.pablo.xue.airplaneapp.model.Usuario;
 import com.pablo.xue.airplaneapp.service.UsuarioService;
 
 @RestController
-@RequestMapping("/api")
 public class UsuarioController {
 	
 	private final UsuarioService userService = new UsuarioService();
@@ -23,11 +22,6 @@ public class UsuarioController {
 	@GetMapping("/user/{id}")
 	public Usuario getUser(@PathVariable String id) {
 		return userService.getUser(id);
-	}
-	
-	@GetMapping("/test")
-	public String hola() {
-		return "hola";
 	}
 	
 }
--- src/main/java/com/pablo/xue/airplaneapp/service/VueloService.java
+++ src/main/java/com/pablo/xue/airplaneapp/service/VueloService.java
@@ -1,6 +1,6 @@
 package com.pablo.xue.airplaneapp.service;
 
-import java.util.Date;
+import java.util.*;
 
 import com.pablo.xue.airplaneapp.Data;
 import com.pablo.xue.airplaneapp.model.Vuelo;
@@ -15,5 +15,13 @@ public class VueloService {
 	public Vuelo getVuelos(int id) {
 		Vuelo vuel1=Data.vuelos.stream().filter(i -> i.getVuelId()==id).findAny().get();
 		return vuel1;
+	}
+	
+	public List<Vuelo> getVuelosByParams(String cOrigen, String cDest, String fechaIni) {
+		List<Vuelo> listAux = new ArrayList<Vuelo>();
+		listAux=Data.vuelos.stream().filter(i -> i.getcOrigen().equals(cOrigen) 
+													&& i.getcDestino().equals(cDest)
+													&& i.getFecIni().toString().equals(fechaIni)).toList();
+		return listAux;
 	}
 }
